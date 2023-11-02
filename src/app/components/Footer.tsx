aa2c7014d1878b6b543c254d699dcf1792d41c4c
Ali <ali.hussein.pre@gmail.com>
2023-11-02 21:34:24 +0100

    chore: initial commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => (
+  <footer className="flex-row-center h-12 border-t border-neutral-200">
+    Footer
+  </footer>
+);
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex-col-center min-h-screen">
+      <div className="container">Sahem</div>
+    </main>
+  );
+}
