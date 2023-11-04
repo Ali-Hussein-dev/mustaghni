fa660960252e9d92dc63242e6b268f183e41fc31
Ali <ali.hussein.pre@gmail.com>
2023-11-04 16:41:36 +0100

    feat: store searches count

---
+++ src/app/api/searches-count/route.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env.mjs"
+import { Redis } from "@upstash/redis"
+
+export const GET = async () => {
+    const redis = new Redis({
+        url: env.UPSTASH_REDIS_REST_URL,
+        token: env.UPSTASH_REDIS_REST_TOKEN
+    })
+    const counts = await redis.get("searches")
+    return Response.json({ counts })
+}
