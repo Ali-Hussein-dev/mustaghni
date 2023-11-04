6a8511fdfa5b2a09ec928f86bc93190dcbc1c56b
Ali <ali.hussein.pre@gmail.com>
2023-11-04 17:02:54 +0100

    feat: add feedback when nothing found

--- src/components/companies-list.tsx
+++ src/components/companies-list.tsx
@@ -5,10 +5,17 @@ export const CompaniesList = ({ companies }: { companies?: Company[] }) => {
   return !companies ? (
     <InitialView />
   ) : (
-    <div className="grid grid-cols-1 gap-4 px-1 md:grid-cols-2">
-      {companies?.map((o, i) => (
-        <DyanmicCompanyCard key={i} title={o.title} logo={o?.logo} />
-      ))}
+    <div>
+      {companies.length === 0 && (
+        <div className="flex-row-center w-full bg-gray-100 py-4 font-semibold">
+          not found
+        </div>
+      )}
+      <div className="grid grid-cols-1 gap-4 px-1 md:grid-cols-2">
+        {companies?.map((o, i) => (
+          <DyanmicCompanyCard key={i} title={o.title} logo={o?.logo} />
+        ))}
+      </div>
     </div>
   );
 };
