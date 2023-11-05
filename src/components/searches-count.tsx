76172936958f4d4a0eb7220d2d7da25a6595d1d2
Ali <ali.hussein.pre@gmail.com>
2023-11-05 16:06:26 +0100

    fix: add proper type

--- src/components/searches-count.tsx
+++ src/components/searches-count.tsx
@@ -2,7 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 
 const getCounts = () => fetch("api/searches-count").then((res) => res.json());
 export const SearchesCount = () => {
-  const { data } = useQuery({ queryKey: ["searches"], queryFn: getCounts });
+  const { data } = useQuery<{ count: number }>({
+    queryKey: ["searches"],
+    queryFn: getCounts,
+  });
 
-  return <p className="text-center">{data?.counts} searches</p>;
+  return <p className="text-center">{data?.count} searches</p>;
 };
