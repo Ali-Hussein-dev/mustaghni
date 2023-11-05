9fa02d84fd06c396f54ab40a8cdb845cf1501320
Ali <ali.hussein.pre@gmail.com>
2023-11-05 19:10:11 +0100

    fix: allow & character for searching

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -3,7 +3,7 @@ import { type NextRequest } from 'next/server'
 import { searchCompanies } from '../../../../sanity/lib/get-companies';
 import { redisClient } from '@/utils/redis';
 
-const nameSchema = z.string().min(1).refine(name => /^[a-zA-Z0-9' ]+$/.test(name), {
+const nameSchema = z.string().min(1).refine(name => /^[a-zA-Z0-9' &]+$/.test(name), {
     message: "name should not contain special characters",
 });
 
