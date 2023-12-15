adf44197a9c78e270690b8f62c96600018f89367
Ali <ali.hussein.pre@gmail.com>
2023-12-15 17:07:00 +0100

    fix: fix layout when showing results

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -13,8 +13,8 @@ export const Hero = () => {
   const { input, setInput, isLoading, companies, onSubmit, inputRef } =
     useSearch();
   return (
-    <div className="mx-auto flex h-screen w-full max-w-3xl flex-col justify-center gap-4">
-      <div className="w-full flex-row-center">
+    <div className="mx-auto flex w-full max-w-3xl flex-col justify-center gap-4">
+      <div className="w-full pt-10 flex-row-center">
         <Image src="/logo.png" alt="logo" width="200" height="75" />
       </div>
       {/* <h1 className="text-center text-3xl font-extrabold text-gray-800">
@@ -31,6 +31,7 @@ export const Hero = () => {
           inputRef={inputRef}
         />
       </form>
+
       <CompaniesList companies={companies} />
     </div>
   );
