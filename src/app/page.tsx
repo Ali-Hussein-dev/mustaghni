8c4265592b2f695e5c06e636131c31dc96a43f83
Ali <ali.hussein.pre@gmail.com>
2023-12-16 15:16:56 +0100

    feat: add new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Hero } from "./components/Hero";
 import { TopSupporters } from "@/components/top-supporters";
+import { WhichBrands } from "@/components/which-brands";
 
 export default function HomePage() {
   return (
     <main className="center container mx-auto h-full w-full grow px-2 py-10 ">
       <Hero />
+      <WhichBrands />
       <TopSupporters />
     </main>
   );
---
+++ src/components/which-brands.tsx
@@ -0,0 +1,40 @@
+const content = {
+  title: "Which Brands To Boycott?",
+  description: "The level of supporting israel varies from brand to brand.",
+  list: [
+    {
+      title: "When brands explicitly support israel",
+      description: "Such as Zara, Facebook, Starbucks ..etc",
+    },
+    {
+      title: "When brands are owned by companies that supports israel",
+      description:
+        "Such as all brands owned by Pepsico, Nestle, Kraft, Unilever, ...etc",
+    },
+    {
+      title: "When brands have business deals with israel",
+      description: "Such as McDonald, HP technology, Amazon, ...etc",
+    },
+  ],
+};
+//======================================
+export const WhichBrands = () => {
+  return (
+    <div className="w-full pt-10 text-gray-700">
+      <div className="mx-auto w-full max-w-3xl">
+        <div className="mb-4 w-full flex-col-center">
+          <h2 className="text-2xl font-extrabold">{content.title}</h2>
+          <p className="">{content.description}</p>
+        </div>
+        <ul className="space-y-2">
+          {content.list.map((item, index) => (
+            <li key={index} className="rounded border px-2 pb-2 pt-4">
+              <h3 className="text-xl font-medium">{item.title}</h3>
+              <p className="font-light">{item.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
