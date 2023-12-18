0812b23d340b44086c7d921875d5fd17ee1eef41
Ali <ali.hussein.pre@gmail.com>
2023-12-18 11:04:19 +0100

    feat: revalidate page at most 1 hour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { TopSupporters } from "@/components/top-supporters";
 import { WhichBrands } from "@/components/which-brands";
 import { InitialView } from "@/components/initial-view";
 
+export const revalidate = 3600; // 1 hour
+
 export default function HomePage() {
   return (
     <main className="center container mx-auto h-full w-full grow px-2 py-10 ">
