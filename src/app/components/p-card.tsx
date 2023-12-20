9c35c789120a2c69e47b8c56614865933ee88690
Ali <ali.hussein.pre@gmail.com>
2023-12-20 17:47:24 +0100

    style: reorder gradient color s

--- src/app/components/p-card.tsx
+++ src/app/components/p-card.tsx
@@ -1,7 +1,7 @@
 //======================================
 const PCard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="rounded bg-gradient-to-tr from-black/70 via-red-700/70 to-green-600/70 p-[2px]">
+    <div className="rounded bg-gradient-to-tr from-green-600/70 via-black/70 to-red-700/70 p-[2px]">
       <div className="rounded bg-white px-4 pb-3 pt-5">{children}</div>
     </div>
   );
