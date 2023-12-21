45b0d8947e69314cd0cb2b0b939b5e3dde28b7a0
Ali <ali.hussein.pre@gmail.com>
2023-12-21 15:45:59 +0100

    feat: add searches count label

--- src/components/searches-count.tsx
+++ src/components/searches-count.tsx
@@ -1,11 +1,16 @@
+import { formatNumber } from "@/utils/format-number";
 import { useQuery } from "@tanstack/react-query";
 
 const getCounts = () => fetch("api/searches-count").then((res) => res.json());
+
 export const SearchesCount = () => {
-  const { data } = useQuery<{ count: number }>({
-    queryKey: ["searches"],
+  const { data } = useQuery<{ searches: number }>({
+    queryKey: ["counts"],
     queryFn: getCounts,
   });
-
-  return <p className="text-center">{data?.count} searches</p>;
+  return data ? (
+    <p className="mb-3 px-1 pt-0 text-center text-lg">
+      {formatNumber(data?.searches, 5)} searches completed
+    </p>
+  ) : null;
 };
