e0dc42404749fbf0e6ea04f7f84e53619c34c347
Ali <ali.hussein.pre@gmail.com>
2023-12-25 23:03:38 +0100

    feat: TikTok link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,16 @@
-// import { FaTiktok } from "react-icons/fa6";
+import { FaTiktok } from "react-icons/fa6";
+// import Link from "next/link";
 
 export const Footer = () => (
   <footer className="h-14 w-full text-gray-800 flex-row-center">
-    <div className="w-full max-w-3xl gap-1 border-t border-neutral-300 py-2 flex-row-center">
-      @{new Date().getFullYear()} <span className="font-bold">Mustaghni</span>
-      {/* {[
+    <div className="w-full max-w-3xl gap-4 border-0 border-t border-solid border-neutral-300 py-2 flex-row-center">
+      <div className="gap-1 flex-row-start">
+        @{new Date().getFullYear()} <span className="font-bold">Mustaghni</span>
+      </div>
+      {[
         // { href: "https://x.com", icon: <FaXTwitter size="20" /> },
         {
-          href: "https://www.tiktok.com/@ali_h_dev",
+          href: "https://www.tiktok.com/@mustaghni_app",
           icon: <FaTiktok size="20" />,
         },
         // { href: "https://tiktok.com", icon: <FaInstagram size="20" /> },
@@ -15,7 +18,10 @@ export const Footer = () => (
         <a className="" key={i} href={o.href} target="_black" rel="noreferrer">
           {o.icon}
         </a>
-      ))} */}
+      ))}
+      {/* <Link href="/about" className="text-gray-800 no-underline">
+        About
+      </Link> */}
     </div>
   </footer>
 );
