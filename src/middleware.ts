5d53fddb38b738cde8402474680f8481190bf487
Ali <ali.hussein.pre@gmail.com>
2023-12-28 21:59:47 +0100

    chore: block access from isr

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import type { NextRequest } from 'next/server'
+
+import { NextResponse } from 'next/server'
+
+// Block Austria, prefer Germany
+const BLOCKED_COUNTRY = 'isr'
+
+// Limit middleware pathname config
+export const config = {
+    matcher: '/',
+}
+
+export function middleware(req: NextRequest) {
+    // Extract country
+    const country = req.geo?.country ?? 'US'
+    console.log("COUNTRY", country)
+
+    // Specify the correct pathname
+    if (country === BLOCKED_COUNTRY) {
+        req.nextUrl.pathname = '/blocked'
+    }
+    // else {
+    //     req.nextUrl.pathname = `/${country}`
+    // }
+    // Rewrite to URL
+    return NextResponse.rewrite(req.nextUrl)
+}
