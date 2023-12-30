98a577d15092375e86774c2a5ba24412a97b6100
Ali <ali.hussein.pre@gmail.com>
2023-12-30 21:29:28 +0100

    feat: add additional field for company schema

--- sanity/schema/company-schema.ts
+++ sanity/schema/company-schema.ts
@@ -1,5 +1,3 @@
-import { type Company } from "@/components/company-card";
-import { client } from "@sanity/lib/client";
 import { defineField } from "sanity";
 const companySchema = {
     name: "company",
@@ -10,26 +8,24 @@ const companySchema = {
             name: "title",
             title: "Title",
             type: "string",
-            validation: (Rule) => {
-                return Rule
-                    .required()
-              .custom(async (title) => {
-                  const query = `*[_type == "company" && title == $title]`;
-                  const params = { title };
-                  const results = await client.fetch<Company[]>(query, params);
-              return results.length === 0 ? true : "Title must be unique";
-          });
+            // validation: (Rule) => {
+            //     return Rule
+            //         // .required()
+            //         .custom(async (title) => {
+            //             const query = `*[_type == "company" && title == $title]`;
+            //             const params = { title };
+            //             const results = await client.fetch<Company[]>(query, params) || [];
+            //             const isTitle = results[0]?.title !== title
+            //             return isTitle ? true : "Title must be unique";
+            //         });
+            // },
+        }), {
+            name: "evidence",
+            title: "Evidence Explanation & Links",
+            type: "array",
+            of: [{ type: "block" }],
         },
-    }),
-      {
-          name: "logo",
-          title: "Brand logo",
-          type: "image",
-          options: {
-              accept: "image/png, image/jpeg, image/jpg",
-          },
-      },
-      {
+        {
           name: "tags",
           title: "Tags",
           type: "array",
@@ -37,13 +33,21 @@ const companySchema = {
       },
       {
           name: "ownedBy",
-          title: "Owner company",
+          title: "owned by",
           type: "string",
       },
       {
           name: "ownerCompanyURL",
           title: "Owner company URL",
           type: "url",
+        },
+        {
+            name: "logo",
+            title: "Brand logo",
+            type: "image",
+            options: {
+                accept: "image/png, image/jpeg, image/jpg",
+            },
         },
     ],
 };
