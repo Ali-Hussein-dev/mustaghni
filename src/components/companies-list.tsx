22fe73bf17ad3cbf9e0a47b7d21a3c1178dbf846
Ali <ali.hussein.pre@gmail.com>
2024-01-02 01:02:42 +0100

    style: handle UI on no results

--- src/components/companies-list.tsx
+++ src/components/companies-list.tsx
@@ -18,39 +18,37 @@ export const CompaniesList = ({
   }
   return (
     <div>
-      {companies.length === 0 && (
+      {companies.length === 0 ? (
         <div className="w-full px-5 font-semibold">
-          <p className="rounded-sm bg-red-50 py-5 text-center text-xl text-red-800">
-            Not found in our database!
+          <p className="rounded-xl border border-solid border-red-700 py-5 text-center text-xl font-medium text-red-900">
+            Not found in database!
           </p>
         </div>
-      )}
-      <div className="overflow-hidden rounded-2xl border border-solid border-gray-200 pb-5 text-gray-700">
-        <div className="mb-4 gap-1 bg-gray-50 px-1 py-2 flex-row-between md:px-5">
-          <div className="w-full grow gap-3 font-medium flex-row-start">
-            Filtered by
-            {tags.length > 0 ? (
-              tags.map((tag) => (
-                <Badge key={tag} variant="light" color="green">
-                  {tag}
-                </Badge>
-              ))
-            ) : (
-              <Badge> NAME</Badge>
-            )}
-          </div>
-
-          {companies.length > 0 && (
+      ) : (
+        <div className="overflow-hidden rounded-2xl border border-solid border-gray-200 pb-5 text-gray-700">
+          <div className="mb-4 gap-1 bg-gray-50 px-2 py-2 flex-row-between md:px-5">
+            <div className="w-full grow gap-3 font-medium flex-row-start">
+              Filtered by
+              {tags.length > 0 ? (
+                tags.map((tag) => (
+                  <Badge key={tag} variant="light" color="green">
+                    {tag}
+                  </Badge>
+                ))
+              ) : (
+                <Badge> NAME</Badge>
+              )}
+            </div>
             <Text className="m-0 w-full p-0 text-right font-medium">
               Found: {companies.length}
             </Text>
-          )}
-        </div>
+          </div>
 
-        <div className="grid grid-cols-1 gap-3 px-1 md:grid-cols-2 md:px-4">
-          {companies?.map((o, i) => <DyanmicCompanyCard key={i} {...o} />)}
+          <div className="grid grid-cols-1 gap-3 px-2 md:grid-cols-2 md:px-4">
+            {companies?.map((o, i) => <DyanmicCompanyCard key={i} {...o} />)}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
