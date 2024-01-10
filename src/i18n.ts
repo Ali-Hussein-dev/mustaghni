f454ae6ea870ab80f8b0808c9fe6b23578020fbc
Ali <ali.hussein.pre@gmail.com>
2024-01-10 16:58:00 +0100

    chore: setup next-intl

---
+++ src/i18n.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { notFound } from "next/navigation";
+import { getRequestConfig } from 'next-intl/server';
+
+// Can be imported from a shared config
+const locales = ['en', 'ar'];
+
+export default getRequestConfig(async (args) => {
+    const { locale } = args;
+    console.log("---------------RUN I18N------------------")
+    // Validate that the incoming `locale` parameter is valid
+    if (!locales.includes(locale as any)) notFound();
+    const home = await import(`../locales/${locale}/home.json`);
+    return {
+        messages: { home: home.default }
+    };
+});
