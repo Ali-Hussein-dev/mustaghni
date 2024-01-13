00af934163656dcd4df4d0610b7eb2326e6362cf
Ali <ali.hussein.pre@gmail.com>
2024-01-13 17:46:22 +0100

    build: fix type error

--- src/app/[locale]/studio/[[...index]]/layout.tsx
+++ src/app/[locale]/studio/[[...index]]/layout.tsx
@@ -1,8 +1,9 @@
 import { unstable_setRequestLocale } from "next-intl/server";
+import type React from "react";
 
 //======================================
-const StudioLayout = ({ children }) => {
-    unstable_setRequestLocale("en");
+const StudioLayout = ({ children }: { children: React.ReactNode }) => {
+  unstable_setRequestLocale("en");
   return children;
 };
 export default StudioLayout;
