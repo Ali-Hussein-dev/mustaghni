843d6f986c9d6a2016d1e1772a37168d90f93f3b
Ali <ali.hussein.pre@gmail.com>
2024-01-13 16:09:09 +0100

    refactor: avoid calling upstash without need

--- src/app/api/filter/route.ts
+++ src/app/api/filter/route.ts
@@ -6,27 +6,27 @@ import { type NextRequest, NextResponse, } from "next/server";
 export const runtime = "edge"
 
 export const GET = async (req: NextRequest) => {
+    const tags = req.nextUrl.searchParams.get("tags");
+    if (!tags) {
+        console.warn("tags params is not truthy", tags)
+        const requestUrl = new URL(req.url)
+        return NextResponse.redirect(requestUrl.origin + "/?msg=tags are not correct")
+    }
     const isRateExceeded = await checkRateLimit(req)
     if (isRateExceeded) {
         return isRateExceeded;
     }
-    const tags = req.nextUrl.searchParams.get("tags");
-    if (tags) {
-        try {
-            await redisClient.incr("searches")
-            const companies = await searchByTags(tags)
-            return new Response(JSON.stringify(companies), { status: 200 })
+    try {
+        await redisClient.incr("searches")
+        const companies = await searchByTags(tags)
+        return new Response(JSON.stringify(companies), { status: 200 })
 
-        } catch (error) {
-            console.error(error);
-            const requestUrl = new URL(req.url)
-            return NextResponse.redirect(requestUrl.origin + "/?msg=something went wrong")
-            // rewrite
-        }
+    } catch (error) {
+        console.error(error);
+        const requestUrl = new URL(req.url)
+        return NextResponse.redirect(requestUrl.origin + "/?msg=something went wrong")
+    // rewrite
     }
-    console.warn("tags params is not truthy", tags)
-    const requestUrl = new URL(req.url)
-    return NextResponse.redirect(requestUrl.origin + "/?msg=tags params is not correct")
     // return new Response(JSON.stringify({ msg: "tags must be provided" }), { status: 400 })
 }
 
