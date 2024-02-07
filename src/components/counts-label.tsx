0e948fa7dfa18d2511948a4e483fd55743e1b5dc
Ali <ali.hussein.pre@gmail.com>
2024-02-07 19:09:01 +0100

    feat: remove the searches count

--- src/components/counts-label.tsx
+++ src/components/counts-label.tsx
@@ -6,7 +6,7 @@ import { MdNumbers } from "react-icons/md";
 const getCounts = () => fetch("api/searches-count").then((res) => res.json());
 
 export const CountsLabel = ({
-  labels: { searches, brands },
+  labels: { brands },
 }: {
   labels: { searches: string; brands: string };
 }) => {
@@ -17,9 +17,9 @@ export const CountsLabel = ({
   return data ? (
     <div className="mb-2 gap-3 rounded-sm bg-black/90 px-2 py-1 text-gray-100 flex-row-start">
       <MdNumbers className="text-red-500" size="17" />
-      <span hidden={!data.searches} className="text-center text-lg">
+      {/* <span hidden={!data.searches} className="text-center text-lg">
         {formatNumber(data?.searches)} {searches}
-      </span>
+      </span> */}
       <span hidden={!data.brands} className="text-center text-lg">
         {formatNumber(data?.brands)} {brands}
       </span>
