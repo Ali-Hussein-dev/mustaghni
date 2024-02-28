21227c529ff9ea81eb9f11e3a91ab4bafb898936
Ali <ali.hussein.pre@gmail.com>
2024-02-28 20:56:54 +0100

    style: make ui consistent

--- src/components/easy-access.tsx
+++ src/components/easy-access.tsx
@@ -1,4 +1,4 @@
-import { Text, Title } from "@mantine/core";
+import { Text } from "@mantine/core";
 import { FaWindows, FaAppStore, FaApple } from "react-icons/fa6";
 import { DiAndroid } from "react-icons/di";
 import { useTranslations } from "next-intl";
@@ -9,10 +9,10 @@ export const EasyAccess = () => {
   return (
     <div className="h-full w-full rounded-[1.7rem] bg-gradient-to-tr from-green-600/70 via-black/70 to-red-700/70 p-[2px] md:max-w-2xl">
       <div className="mx-auto w-full rounded-[1.6rem] bg-white pb-8 pt-10">
-        <div className="mx-auto max-w-fit">
-          <Title order={2} ta="center">
+        <div className="mx-auto max-w-fit text-gray-700">
+          <h2 className="text-center text-3xl font-extrabold text-gray-700">
             {t("easyAccess.title")}
-          </Title>
+          </h2>
           <Text ta="center">{t("easyAccess.description")}</Text>
         </div>
         <div className="gap-8 pt-6 flex-row-center">
